aa6952d7b3c85e8cdc2437f3a6e2175928b4141d
Emanoel <emchavesjr@gmail.com>
2020-06-02 14:31:29 -0300

    Add screens. Pending work of them

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+
+export default function ProductOverviewScreen(props){
+	return(
+		<FlatList>
+				
+		</FlatList>
+	)
+}
+
+const styles = StyleSheet.create({
+	
+})
